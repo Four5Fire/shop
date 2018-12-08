092eb350b7f8790773ae8d5e97df422812982d5a
Four5Fire <942558046@qq.com>
2018-12-08 14:56:34 +0800

    12.8服务器配置

--- src/main/java/cn/playcall/shop/demo/config/MyPicConfig.java
+++ src/main/java/cn/playcall/shop/demo/config/MyPicConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyPicConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/user/**").addResourceLocations("file:/home/caojunyi/Desktop/文件管理/代码管理/Java/shop/src/main/resources/static/user/");
-        registry.addResourceHandler("/shopUser/**").addResourceLocations("file:/home/caojunyi/Desktop/文件管理/代码管理/Java/shop/src/main/resources/static/shopUser/");
+//        registry.addResourceHandler("/user/**").addResourceLocations("file:/home/caojunyi/Desktop/文件管理/代码管理/Java/shop/src/main/resources/static/user/");
+//        registry.addResourceHandler("/shopUser/**").addResourceLocations("file:/home/caojunyi/Desktop/文件管理/代码管理/Java/shop/src/main/resources/static/shopUser/");
+        registry.addResourceHandler("/user/**").addResourceLocations("file:/home/shop/src/main/resources/static/user/");
+        registry.addResourceHandler("/shopUser/**").addResourceLocations("file:/home/shop/src/main/resources/static/shopUser/");
     }
 }
